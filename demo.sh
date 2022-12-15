f4311d46fd730f585c907ee19881d6e4f35e25ae
yathamuday <yathamuday@gmail.com>
2022-12-15 11:54:07 +0530

    creating a file

--- demo.sh
+++ demo.sh
@@ -1,8 +1,6 @@
-echo "***********this command display the root f.s space**********
-
+echo "***********this command display the root f.s space**********"
 df -h
-
-echo "*********** this command shows the today date ******
-
-
+echo "*********** this command shows the today date ******"
 date
+echo "******** this is free space command ********"
+free -m
