cc542a66b27b0ee3bcf5e2773a3c76b8987b0501
yathamuday <yathamuday@gmail.com>
2022-12-15 11:39:04 +0530

    delete

--- demo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "********* Display root F.S Space *****"
-df -h
-echo "********* Display Today date *******"
-date
-echo " ******* Display jenkins memory space *******"
-free -m
-echo "******** this is a testing file *******"
