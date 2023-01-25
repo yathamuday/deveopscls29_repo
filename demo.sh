be9eddfd6f39fef2a4189047f4a38e74eb59f29a
yathamuday <yathamuday@gmail.com>
2023-01-25 09:06:03 +0530

    message

--- demo.sh
+++
@@ -1,6 +0,0 @@
-echo "***********this command display the root f.s space**********"
-df -h
-echo "*********** this command shows the today date ******"
-date
-echo "******** this is free space command ********"
-free -m
