97e8a45c4dbb9c3e401a2dc265734ac41644a248
yathamuday <yathamuday@gmail.com>
2023-01-25 09:10:12 +0530

    message

---
+++ demo.sh
@@ -0,0 +1,6 @@
+echo " ************This comand display the free space *****" 
+df -h
+echo "************** This command display the today date ***** "
+date 
+echo " **************** This command dispaly the free space ***** "
+free 
