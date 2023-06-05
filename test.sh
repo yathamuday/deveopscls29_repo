b54fca8a8023abb246ca9756b3b1263b0bd86a48
yathamuday <yathamuday@gmail.com>
2023-06-05 20:58:52 +0530

    public key

--- test.sh
+++
@@ -1,3 +0,0 @@
-this is testing for git 
-developer added some data
-developer 3 is added some new data  
--- uday.sh
+++
@@ -1,8 +0,0 @@
-echo ********** This is command display disk free space *******
-df -h
-echo ********** This command display memory space *********
-free -m
-echo ********** This command dispaly today date********
-date
-echo ********** This command disply cpu status******
-df
