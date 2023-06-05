00e6018013c5bc25b5e0d75aba27d52a71c92b14
yathamuday <yathamuday@gmail.com>
2023-06-05 20:55:17 +0530

    deleting

--- demo.sh
+++
@@ -1,8 +0,0 @@
-echo " ************This comand display the free space *****" 
-df -h
-echo "************** This command display the today date ***** "
-date 
-echo " **************** This command dispaly the free space ***** "
-free
-echo " ********** this is a file *********"
-
